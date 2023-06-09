3abeb973adfcb6430406fff78e25151408ae54e9
Tarek Munshi <tarekmunshi.13@gmail.com>
2023-06-09 17:03:06 +0600

    u

--- config/plugins.js
+++ config/plugins.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
                     name: 'lustrous-melba-53495c',
                     id: "b4d3dab2-4edd-4f2d-9bc1-aa66fbb44cfb",
                     buildHook: "https://api.netlify.com/build_hooks/648305965213fe120f0ab729",
-                    branch: 'master' // optional
+                    branch: 'main' // optional
                 }
             ]
         },
